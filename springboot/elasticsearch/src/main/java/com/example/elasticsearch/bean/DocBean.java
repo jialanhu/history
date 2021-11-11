251eeeb9d8b438a5ff8cb9f345d91f110c999033
jialan <584856386@qq.com>
2021-11-11 15:22:28 +0800

    [ADD]SpringBoot使用ES

---
+++ springboot/elasticsearch/src/main/java/com/example/elasticsearch/bean/DocBean.java
@@ -0,0 +1,64 @@
+package com.example.elasticsearch.bean;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+
+@Document(indexName = "doc")
+public class DocBean {
+
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Keyword)
+    private String title;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word")
+    private String content;
+
+    @Field(type = FieldType.Integer)
+    private Integer type;
+
+    public DocBean () {}
+
+    public DocBean(Long id, String title, String content, Integer type) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
