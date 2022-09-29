70aff2ac63709d31caef65056d211d803d0567bb
JiaLan <sakaketsukihana@github.com>
2022-09-29 16:13:51 +0800

    [ADD] debug 观察 ioredis的行为 [ADD] mqemitter-redis 使用 pub/sub 进行集群模式 [ADD] aedes-persistence-redis 进行qos 与 retained 的消息持久化

--- nodejs/aedes/src/index.ts
+++ nodejs/aedes/src/index.ts
@@ -1,9 +1,24 @@
-
 import * as net from "net";
 import { mqttAuthenticate, mqttPreConnect } from "./mqtt/mqttHandle";
 import { mqttClientError, mqttClientReady, mqttConnectionError } from "./mqtt/mqttEvent";
 
-const aedes = require("aedes")();
+const redisConfig = {
+  port: 6379,
+  host: "localhost",
+  password: "123456",
+  db: 0,
+}
+
+const aedes = require("aedes")({
+  mq: require("mqemitter-redis")(redisConfig),
+  persistence: require("aedes-persistence-redis")({
+    ...redisConfig,
+    maxSessionDelivery: 100, // maximum offline messages deliverable on client CONNECT, default is 1000
+    packetTTL: function (packet) { // offline message TTL, default is disabled
+      return 10 //seconds
+    }
+  })
+});
 const mqttServer = net.createServer(aedes.handle);
 mqttServer.listen(1883);
 
