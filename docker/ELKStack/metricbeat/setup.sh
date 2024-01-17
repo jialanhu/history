9bc6a72053c3a63f1800e97acd96ef114a966c33
Jialan <>
2024-01-17 10:52:29 +0800

    fixed metricbeat/setup.sh

--- docker/ELKStack/metricbeat/setup.sh
+++ docker/ELKStack/metricbeat/setup.sh
@@ -3,12 +3,14 @@
 
 ## metricbeat setup -e;
 ## -e is optional and sends output to standard error instead of the configured log output.
-docker compose up -d metricbeat # refresh env
-docker cp metricbeat/metricbeat.yml metricbeat:/usr/share/metricbeat/metricbeat.yml
+docker compose up -d metricbeat; # refresh env
+docker cp metricbeat/metricbeat.yml metricbeat:/usr/share/metricbeat/metricbeat_new.yml;
 docker exec metricbeat bash -c '
     cd /usr/share/metricbeat;
-    chown root metricbeat.yml;
+    chown root metricbeat_new.yml;
+    cp metricbeat_new.yml metricbeat.yml;
+    rm metricbeat_new.yml;
     metricbeat test config;
     metricbeat modules enable docker;
     metricbeat setup -e;
-'
+';
