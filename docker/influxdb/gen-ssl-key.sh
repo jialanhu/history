b79a7cce946d2122d121a274d4868984bcaa75b7
Jialan <>
2024-01-26 14:14:41 +0800

    fix gen-ssl-key.sh

--- docker/influxdb/gen-ssl-key.sh
+++ docker/influxdb/gen-ssl-key.sh
@@ -1,1 +1,1 @@
-openssl req -x509 -nodes -newkey rsa:2048 -keyout ./influxdb-selfsigned.key -out ./influxdb-selfsigned.crt -days 365
+openssl req -x509 -nodes -newkey rsa:2048 -keyout influxdb-selfsigned.key -out influxdb-selfsigned.crt -days 365
