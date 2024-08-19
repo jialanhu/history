46d2856e77c409bb2ed49f5837bf67af205a62ac
Jialan <>
2024-08-19 14:22:49 +0800

    imp nginx gen-ssl-key.sh

--- docker/mysql/ssl/gen-ssl-key.sh
+++ docker/mysql/ssl/gen-ssl-key.sh
@@ -1,6 +1,6 @@
 openssl req -x509 -nodes -newkey rsa:2048 -keyout ca-key.pem -out ca.pem -days 3650 -subj "/CN=MySQL_Server_8.0.36_Auto_Generated_CA_Certificate";
 openssl req -SHA-256 -nodes -newkey rsa:2048 -keyout server-key.pem -out server-req.pem -days 3650 -subj "/CN=MySQL_Server_8.0.36_Auto_Generated_Server_Certificate";
-openssl req -SHA-256 -nodes -newkey rsa:2048 -keyout client-key.pem -out client-req.pem -days 3650 -subj "/CN=MySQL_Server_8.0.36_Auto_Generated_Server_Certificate";
+openssl req -SHA-256 -nodes -newkey rsa:2048 -keyout client-key.pem -out client-req.pem -days 3650 -subj "/CN=MySQL_Client_8.0.36_Auto_Generated_Server_Certificate";
 openssl x509 -req -in server-req.pem -days 365 -CA ca.pem -CAkey ca-key.pem -set_serial 01 -out server-cert.pem
 openssl x509 -req -in client-req.pem -days 365 -CA ca.pem -CAkey ca-key.pem -set_serial 01 -out client-cert.pem
 
--- docker/nginx/gen-ssl-key.sh
+++ docker/nginx/gen-ssl-key.sh
@@ -1,4 +1,9 @@
 mkdir -p ./certs/
-openssl genpkey -algorithm RSA -out ./certs/private.key
-openssl req -new -key ./certs/private.key -out ./certs/csr.pem
-openssl req -x509 -sha256 -days 365 -key ./certs/private.key -in ./certs/csr.pem -out ./certs/certificate.crt
+
+openssl req -x509 -nodes -newkey rsa:2048 -keyout ./certs/ca-key.pem -out ./certs/ca.pem -days 3650 -subj "/CN=Localhost_CA_Certificate";
+openssl req -SHA-256 -nodes -newkey rsa:2048 -keyout ./certs/server-key.pem -out ./certs/server-req.pem -days 3650 -subj "/CN=localhost" -addext "subjectAltName=DNS:localhost";
+openssl req -SHA-256 -nodes -newkey rsa:2048 -keyout ./certs/client-key.pem -out ./certs/client-req.pem -days 3650 -subj "/CN=localhost" -addext "subjectAltName=DNS:localhost";
+openssl x509 -req -in ./certs/server-req.pem -days 365 -CA ./certs/ca.pem -CAkey ./certs/ca-key.pem -set_serial 01 -out ./certs/server-cert.pem
+openssl x509 -req -in ./certs/client-req.pem -days 365 -CA ./certs/ca.pem -CAkey ./certs/ca-key.pem -set_serial 01 -out ./certs/client-cert.pem
+
+openssl verify -CAfile ./certs/ca.pem ./certs/server-cert.pem ./certs/client-cert.pem;
